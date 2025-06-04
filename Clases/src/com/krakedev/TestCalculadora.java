b843395d8292fbef59fbd158624c084f0dcd023b
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-04 16:52:21 -0500

    se añadio y comprobo el metodo promedio

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -10,16 +10,19 @@ public class TestCalculadora {
 		int resultadoResta;
 		double resultadoMultiplicacion;
 		double resultadoDivision;
+		double resultadoPromedio;
 	
 		resultadoSuma=calcu.sumar(5,3);
 		resultadoResta=calcu.restar(10,3);
 		resultadoMultiplicacion=calcu.multiplicar(3, 5);
 		resultadoDivision=calcu.division(10, 2);
+		resultadoPromedio=calcu.promedio(10, 2,8);
 		
 		System.out.println("RESULTADO SUMA: "+resultadoResta);
 		System.out.println("RESULTADO RESTA: "+resultadoSuma);
 		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
 		System.out.println("RESULTADO DIVICION: "+resultadoDivision);
+		System.out.println("RESULTADO PROMEDIO: "+resultadoPromedio);
 	}
 
 }
