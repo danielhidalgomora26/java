3af802c7addabf2972870db149d0358c54eaa52f
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-04 16:50:45 -0500

    se añadio y comprobo el metodo division

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -9,14 +9,17 @@ public class TestCalculadora {
 		int resultadoSuma;
 		int resultadoResta;
 		double resultadoMultiplicacion;
+		double resultadoDivision;
 	
 		resultadoSuma=calcu.sumar(5,3);
 		resultadoResta=calcu.restar(10,3);
 		resultadoMultiplicacion=calcu.multiplicar(3, 5);
+		resultadoDivision=calcu.division(10, 2);
 		
 		System.out.println("RESULTADO SUMA: "+resultadoResta);
 		System.out.println("RESULTADO RESTA: "+resultadoSuma);
 		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+		System.out.println("RESULTADO DIVICION: "+resultadoDivision);
 	}
 
 }
