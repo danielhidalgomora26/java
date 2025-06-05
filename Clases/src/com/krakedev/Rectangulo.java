b9a63324391cf23924ae2e9b64892634b5be47bf
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-05 10:02:11 -0500

    se realizo las clases rectangulo y cuadrado con sus respectivos argumentos y metodos

---
+++ Clases/src/com/krakedev/Rectangulo.java
@@ -0,0 +1,16 @@
+package com.krakedev;
+
+public class Rectangulo {
+	public int base;
+	public int altura;
+	
+	public int calcularArea() {
+		int area=base*altura;
+		return area;
+	}
+	public double calcularPerimetro() {
+		double perimetro;
+		perimetro=(base+base)+(altura+altura);
+		return perimetro;
+	}
+}
---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,16 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		Cuadrado c1=new Cuadrado();
+		double areac1;
+		double perimetroc1;
+		c1.lado=5;
+		areac1=c1.calcularArea();
+		perimetroc1=c1.calcularPerimetro();
+		System.out.println("Area de c1: "+areac1);
+		System.out.println("Perimetro de c1: "+perimetroc1);
+	}
+
+}
