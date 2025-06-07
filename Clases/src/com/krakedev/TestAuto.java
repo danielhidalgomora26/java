a7e493f4126b27367d4e9ace2fa82aa3e4fd758e
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-07 11:19:08 -0500

    se agrego el private a la clase Auto

--- Clases/src/com/krakedev/TestAuto.java
+++ Clases/src/com/krakedev/TestAuto.java
@@ -6,22 +6,23 @@ public class TestAuto {
 		Auto auto1= new Auto();
 		Auto auto2= new Auto();
 		
+		auto1.setMarca("Kia");
+		auto1.setAnio(2007);
+		auto1.setPrecio(1200.500);
 		
-		auto1.marca="Kia";
-		auto1.anio=2007;
-		auto1.precio=1200.500;
+		auto2.setMarca("Chevrolet");
+		auto2.setAnio(2005);
+		auto2.setPrecio(1500.100);
 		
-		auto2.marca="Chevrolet";
-		auto2.anio=2005;
-		auto2.precio=1500.100;
+		System.out.println("marca 1: "+auto1.getMarca());
+		System.out.println("anio 1: "+auto1.getAnio());
+		System.out.println("precio 1: "+auto1.getAnio());
 		
-		System.out.println("marca 1: "+auto1.marca);
-		System.out.println("anio 1: "+auto1.anio);
-		System.out.println("precio 1: "+auto1.precio);
 		System.out.println("-------------------");
-		System.out.println("marca 2: "+auto2.marca);
-		System.out.println("anio 2: "+auto2.anio);
-		System.out.println("precio 2: "+auto2.precio);
+		
+		System.out.println("marca 2: "+auto2.getMarca());
+		System.out.println("anio 2: "+auto2.getAnio());
+		System.out.println("precio 2: "+auto2.getAnio());
 	}
 
 }
