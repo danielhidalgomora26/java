81aa7d940f8edd4dddcac1dd6a135d5b364b603c
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-07 11:28:58 -0500

    se agrego el private a la clase Persona

--- Clases/src/com/krakedev/TestPersona.java
+++ Clases/src/com/krakedev/TestPersona.java
@@ -9,20 +9,20 @@ public class TestPersona {
 		Persona p2 = new Persona();
 
 		// 3. Modificar los atributos
-		p.nombre = "Mario";
-		p.edad = 45;
-		p.estatura = 1.56;
+		p.setNombre("Mario");
+		p.setEdad(45);
+		p.setEstatura(1.56);
 
 		// 4. Accedo a los atributos
-		System.out.println("nombre: " + p.nombre);
-		System.out.println("edad: " + p.edad);
-		System.out.println("edad: " + p.estatura);
+		System.out.println("nombre: " + p.getNombre());
+		System.out.println("edad: " + p.getEdad());
+		System.out.println("edad: " + p.getEstatura());
 
-		p2.nombre = "Angelina";
+		p2.setNombre("Angelina");
 
 		System.out.println("*********");
-		System.out.println("p.nombre: " + p.nombre);
-		System.out.println("p2.nombre: " + p2.nombre);
+		System.out.println("p.nombre: " + p.getNombre());
+		System.out.println("p2.nombre: " + p2.getNombre());
 
 	}
 
