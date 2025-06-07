3e0a94e46b6c8d186db5b7dc5180a167eb1472a1
danielhidalgomora26 <danielhidalgomora@gmail.com>
2025-06-07 11:23:00 -0500

    se agrego el private a la clase Cuadrado

--- Clases/src/com/krakedev/TestCuadrado.java
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -6,7 +6,7 @@ public class TestCuadrado {
 		Cuadrado c1=new Cuadrado();
 		double areac1;
 		double perimetroc1;
-		c1.lado=5;
+		c1.setLado(5);
 		areac1=c1.calcularArea();
 		perimetroc1=c1.calcularPerimetro();
 		System.out.println("Area de c1: "+areac1);
